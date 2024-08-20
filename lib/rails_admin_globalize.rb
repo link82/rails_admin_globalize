c03fa8d5fff8e45d4d757e428ed0c6433826e6a1
Davide Cenzi <link82@gmail.com>
2024-08-20 10:21:30 +0200

    partially fixed

--- lib/rails_admin_globalize.rb
+++ lib/rails_admin_globalize.rb
@@ -50,7 +50,8 @@ module RailsAdmin
               ::Globalize.with_locale params[:target_locale] do
                 sanitize_params_for!(:update)
 
-                @object.set_attributes(params[@abstract_model.param_key])
+                # @object.set_attributes(params[@abstract_model.param_key])
+                @object.assign_attributes(params[@abstract_model.param_key])
                 @authorization_adapter && @authorization_adapter.attributes_for(:update, @abstract_model).each do |name, value|
                   @object.send("#{name}=", value)
                 end
